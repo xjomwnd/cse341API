46a28ae3f5ad6a2fc995bf5348b7be554a748b00
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-29 14:16:55 +0300

    commit

--- server.js
+++ server.js
@@ -1,33 +1,6 @@
-// Import dependencies
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const app = require('./app');
 const mongoose = require('mongoose');
-
-// Import configuration
 const config = require('./config');
-
-// Import middleware
-const sessionMiddleware = require('./middlewares/sessionMiddleware');
-const authMiddleware = require('./middlewares/authMiddleware');
-const errorMiddleware = require('./middlewares/errorMiddleware');
-
-// Import routes
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-// Create Express app
-const app = express();
-
-// Middleware
-app.use(bodyParser.json());
-app.use(sessionMiddleware);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cors());
 
 // Database connection
 mongoose.connect(config.database.uri, {
@@ -36,24 +9,6 @@ mongoose.connect(config.database.uri, {
 })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.error('MongoDB Connection Error:', err));
-
-// Passport configuration
-require('./config/passport')(passport);
-
-// Routes
-app.use('/auth', authRoutes);
-app.use('/users', authMiddleware, userRoutes);
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Index route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-// Error handling middleware
-app.use(errorMiddleware);
 
 // Start server
 const PORT = process.env.PORT || 3000;
