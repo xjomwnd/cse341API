500cbbc4aedd38464ae6a7ad5feafc1378006d80
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-29 19:25:01 +0300

    commit

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,6 @@
 // Import required modules
 const express = require('express');
 const router = express.Router();
-const { validateComment } = require('../middleware/validation'); // Import validateComment middleware
 
 // Define route handlers
 router.post('/:postId', validateComment, async (req, res) => {
