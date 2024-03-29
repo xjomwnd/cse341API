340b8b0a30cbc6f7e0302010f9adcf1e10939cb5
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-29 12:12:35 +0300

    commit

---
+++ utils/errorHandling.js
@@ -0,0 +1,19 @@
+// errorHandling.js
+
+// Function to handle errors
+function errorHandler(err, req, res, next) {
+    // Log the error
+    console.error(err.stack);
+
+    // Set response status code
+    res.status(500);
+
+    // Return error response
+    res.json({
+        error: {
+            message: err.message || 'Internal Server Error'
+        }
+    });
+}
+
+module.exports = errorHandler;
