77f8a12857e742728e1ae9b3c79a3460d3397fb0
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-29 22:00:54 +0300

    commit

--- models/comment.js
+++ models/comment.js
@@ -1,35 +1,9 @@
-// Import required modules
 const express = require('express');
 const router = express.Router();
-const comments = require('../models/comment');
-const { validateComment } = require('../middleware/validation'); // Import validateComment middleware
 
-// Define route handlers
-
-// Get comments for a post
-router.get('/:postId', async (req, res) => {
-    try {
-        const comments = await Comment.find({ postId: req.params.postId });
-        res.json(comments);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+// Define POST route for comments
+router.post('/comments', (req, res) => {
+  // Handle the POST request here
 });
 
-// Add a comment to a post
-router.post('/:postId', validateComment, async (req, res) => {
-    const comment = new comment({
-        postId: req.params.postId,
-        userId: req.body.userId,
-        text: req.body.text
-    });
-    try {
-        const newComment = await comment.save();
-        res.status(201).json(newComment);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-// Export router
 module.exports = router;
