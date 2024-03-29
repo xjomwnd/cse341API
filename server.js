d621c69d094980a22b69e62d50f9a682269f965b
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-29 13:35:37 +0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const OAuth2Strategy = require('passport-oauth2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
