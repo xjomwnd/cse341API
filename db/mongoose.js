852284b592f49b1e8fd47ca24194bf4a105c2a45
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-03-31 02:04:22 +0300

    commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,20 +1,15 @@
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 const config = require('config');
 
 // Access configuration properties
 const mongodbUri = config.get('mongodb.uri');
 
-module.exports = {
-    mongodb: {
-        uri: 'mongodb+srv;//ndimong:<password>@cluster0.iwufs.mongodb.net/'
-    }
-};
-
 // MongoDB connection URI
-const uri = config.mongodb.uri; mongodb+srv;//ndimong:<password>@cluster0.iwufs.mongodb.net/
+// const uri = config.mongodb.uri; mongodb+srv;//ndimong:<password>@cluster0.iwufs.mongodb.net/
+// ^ This line is commented out as it's not needed and contains syntax errors
 
 // Connect to MongoDB
-mongoose.connect(uri, {
+mongoose.connect(mongodbUri, { // Use mongodbUri variable instead of uri
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -24,3 +19,5 @@ mongoose.connect(uri, {
 });
 
 // Define Schema and Models here if using Mongoose
+
+module.exports = mongoose; // Optionally, you can export mongoose for use in other modules
