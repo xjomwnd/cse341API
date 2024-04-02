d176fca68ac0125ad9ebd8b7d2f6642045470567
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-02 07:29:31 +0300

    commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 
 // MongoDB connection string
-const mongodbUri = 'mongodb+srv://ndimong:<password>@cluster0.iwufs.mongodb.net/';
+const mongodbUri = ' mongodb+srv://mwa21001:<password>@cluster0.dmvu7jc.mongodb.net/';
 
 // Connect to MongoDB
-mongoose.connect(mongodbUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(mongodbUri)
+  .then(() => {
     console.log('Connected to MongoDB');
-}).catch(error => {
+  })
+  .catch(error => {
     console.error('Error connecting to MongoDB:', error);
-});
+  });
 
 module.exports = mongoose;
