08572b5c57d48a5e6f9b7500a977ddff3a0e4371
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-02 21:17:46 +0300

    commit

--- db/connect.js
+++ db/connect.js
@@ -1,26 +1,9 @@
 const mongoose = require('mongoose');
 
-// Load environment variables from .env file
-require('dotenv').config();
-
-// MongoDB connection string from environment variable
-const MONGODB_URI = process.env.MONGODB_URI;
+// MongoDB connection string
+const uri = 'mongodb+srv://ndimong:<password>@cluster0.iwufs.mongodb.net/';
 
 // Connect to MongoDB
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-// Get the default connection
-const db = mongoose.connection;
-
-// Event listeners for database connection
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-// Export the database connection
-module.exports = db;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
